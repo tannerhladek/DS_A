65ccfb3a16898f30ed6e4f6f9d81f9aab1a63795
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-02-24 16:55:44 -0800

    136 Single Number JS

---
+++ leetCode/136_singleNumber.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+   let counter = {};
+
+   for (let i = 0; i < nums.length; i++) {
+      let currNum = nums[i];
+      if (currNum in counter) {
+         counter[currNum]++;
+      } else {
+         counter[currNum] = 1
+      }
+   }
+
+   for (let i = 0; i < nums.length; i++) {
+      let currNum = nums[i];
+      if (counter[currNum] === 1) return currNum
+   }
+};
