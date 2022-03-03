f467e4f46b5557627f2bd82fa3f66be34bda9f5a
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-03-03 15:14:12 -0800

    refined 2 length of longest sub string JS

--- leetCode/3_lengthOfLongestSubstring.js
+++ leetCode/3_lengthOfLongestSubstring.js
@@ -2,23 +2,52 @@
  * @param {string} s
  * @return {number}
  */
+
+// most efficient solution (pointers never move backwards)
 var lengthOfLongestSubstring = function (s) {
-   let l = r = sum = maxSum = 0;
-   let tracker = new Set();
+   let l = r = maxSum = 0;
+   let tracker = {};
 
    while (r < s.length) {
-      if (!tracker.has(s[r])) {
-         tracker.add(s[r]);
-         sum += 1;
-         maxSum = Math.max(maxSum, sum)
-         r += 1;
+      let char = s[r];
+      if (!(char in tracker)) {
+         tracker[char] = r;
       } else {
-         l += 1;
-         r = l;
-         sum = 0;
-         tracker = new Set();
+         l = Math.max(l, tracker[char] + 1);
+         tracker[char] = r
       }
+      maxSum = Math.max(maxSum, r - l + 1)
+      r++;
    }
-
    return maxSum
 };
+
+
+
+
+
+
+
+
+
+// // using sliding window
+// var lengthOfLongestSubstring = function (s) {
+//    let l = r = sum = maxSum = 0;
+//    let tracker = new Set();
+
+//    while (r < s.length) {
+//       if (!tracker.has(s[r])) {
+//          tracker.add(s[r]);
+//          sum += 1;
+//          maxSum = Math.max(maxSum, sum)
+//          r += 1;
+//       } else {
+//          l += 1;
+//          r = l;
+//          sum = 0;
+//          tracker = new Set();
+//       }
+//    }
+
+//    return maxSum
+// };
