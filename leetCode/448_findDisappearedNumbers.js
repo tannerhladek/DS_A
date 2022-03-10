da8d6db1bd9f09b9212ec19b069bd4c8e0c75ef0
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-03-09 16:11:59 -0800

    solved 448 find disappearing number in constant space JS

--- leetCode/448_findDisappearedNumbers.js
+++ leetCode/448_findDisappearedNumbers.js
@@ -1,11 +1,34 @@
+// O(1) time and space
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 var findDisappearedNumbers = function (nums) {
-   const numSet = new Set(nums);
-   const result = [];
 
-   for (let i = 1; i <= nums.length; i++) {
-      if (numSet.has(i)) continue
-      else result.push(i)
-   };
+   for (let i = 0; i < nums.length; i++) {
+      let index = Math.abs(nums[i]) - 1;
+      if (nums[index] > 0) {
+         nums[index] = -nums[index]
+      }
+   }
 
-   return result
+   let res = [];
+   nums.forEach((num, i) => {
+      if (num > 0) res.push(i + 1)
+   })
+
+   return res
 };
+
+// // O(n) space
+// var findDisappearedNumbers = function (nums) {
+//    const numSet = new Set(nums);
+//    const result = [];
+
+//    for (let i = 1; i <= nums.length; i++) {
+//       if (numSet.has(i)) continue
+//       else result.push(i)
+//    };
+
+//    return result
+// };
