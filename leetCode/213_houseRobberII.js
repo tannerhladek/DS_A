d38450c041e7590fff561e63239064315af821f9
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-04-01 16:22:00 -0700

    leetcode house robber 2

--- leetCode/213_houseRobberII.js
+++ leetCode/213_houseRobberII.js
@@ -1,3 +1,31 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var rob = function (nums) {
+//    if (nums.length === 1) return nums[0]
+
+//    let n1 = _rob1(nums.slice(0, nums.length - 1))
+//    let n2 = _rob1(nums.slice(1))
+
+//    return Math.max(n1, n2)
+// };
+
+// // rob non-circle array
+// let _rob1 = (nums, i = 0, memo = {}) => {
+
+//    if (i in memo) return memo[i]
+//    if (i >= nums.length) return 0
+
+//    let include = nums[i] + _rob1(nums, i + 2, memo)
+//    let exclude = _rob1(nums, i + 1, memo)
+//    let max = Math.max(include, exclude)
+
+//    memo[i] = max
+//    return memo[i]
+// }
+
+// round 2
 /**
  * @param {number[]} nums
  * @return {number}
@@ -5,22 +33,19 @@
 var rob = function (nums) {
    if (nums.length === 1) return nums[0]
 
-   let n1 = _rob1(nums.slice(0, nums.length - 1))
-   let n2 = _rob1(nums.slice(1))
+   let opt1 = _rob(nums.slice(1))
+   let opt2 = _rob(nums.slice(0, nums.length - 1))
 
-   return Math.max(n1, n2)
+   return Math.max(opt1, opt2)
 };
 
-// rob non-circle array
-let _rob1 = (nums, i = 0, memo = {}) => {
-
+const _rob = (nums, i = 0, memo = {}) => {
    if (i in memo) return memo[i]
    if (i >= nums.length) return 0
 
-   let include = nums[i] + _rob1(nums, i + 2, memo)
-   let exclude = _rob1(nums, i + 1, memo)
-   let max = Math.max(include, exclude)
+   let include = nums[i] + _rob(nums, i + 2, memo)
+   let exclude = _rob(nums, i + 1, memo)
 
-   memo[i] = max
+   memo[i] = Math.max(include, exclude)
    return memo[i]
 }
