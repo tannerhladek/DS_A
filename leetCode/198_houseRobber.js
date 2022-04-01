9bbbb7cc376aa1aa7456b6180a9c11ee43ad881c
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-04-01 16:16:22 -0700

    leetcode house robber round 2

--- leetCode/198_houseRobber.js
+++ leetCode/198_houseRobber.js
@@ -1,16 +1,28 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var rob = function (nums, i = 0, memo = {}) {
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var rob = function (nums, i = 0, memo = {}) {
 
+//    if (i in memo) return memo[i]
+//    if (i >= nums.length) return 0
+
+//    let include = nums[i] + rob(nums, i + 2, memo)
+//    let exclude = rob(nums, i + 1, memo)
+//    let max = Math.max(include, exclude)
+
+//    memo[i] = max
+//    return memo[i]
+// };
+
+//round 2
+var rob = function (nums, i = 0, memo = {}) {
    if (i in memo) return memo[i]
    if (i >= nums.length) return 0
 
    let include = nums[i] + rob(nums, i + 2, memo)
    let exclude = rob(nums, i + 1, memo)
-   let max = Math.max(include, exclude)
 
-   memo[i] = max
+   memo[i] = Math.max(include, exclude)
    return memo[i]
 };
